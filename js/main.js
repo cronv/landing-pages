cc09ae85f8365a5e780a2304ecebefe8e04d4796
cronv <mister.swim@yandex.ru>
2024-09-16 11:55:29 +0300

    Fix design #1

--- js/main.js
+++ js/main.js
@@ -1,105 +1,101 @@
-const imageIndexes = [
-    "LS5A4548.jpg",
-    "LS5A4553.jpg",
-    "LS5A4562.jpg",
-    "LS5A4564.jpg",
-    "LS5A4583.jpg",
-    "LS5A4596.jpg",
-    "LS5A4600.jpg",
-    "LS5A4603.jpg",
-    "LS5A4624.jpg",
-    "LS5A4630.jpg",
-    "LS5A4634.jpg",
-    "LS5A4638.jpg",
-    "LS5A4640.jpg",
-    "LS5A4642.jpg",
-    "LS5A4655.jpg",
-    "LS5A4666.jpg",
-    "LS5A4693.jpg",
-    "LS5A4694.jpg",
-    "LS5A4708.jpg",
-    "LS5A4716.jpg",
-    "LS5A4751.jpg",
-    "LS5A4763.jpg",
-    "LS5A4770.jpg",
-    "LS5A4784.jpg",
-    "LS5A4785.jpg",
-    "LS5A4787.jpg",
-    "LS5A4789.jpg",
-    "LS5A4803.jpg"
-];
+import { imageIndexes } from './data/imageData.js';
 
-document.getElementById("loading").style.display = "flex";
-window.addEventListener("load", function() {
-    document.getElementById("loading").style.display = "none";
+var main = {
+    imageIndexes: imageIndexes,
 
-    // ultra img
-    document.querySelectorAll('img').forEach(image => {
-        image.addEventListener('click', function() {
-            const cardParent = this.closest('.card');
+    init: function() {
+        this.showLoading();
+        this.setupImageModals();
+        this.setupSearch();
+        // this.addItems();
+    },
 
-            if (!cardParent) {
-                return;
-            }
+    showLoading: function() {
+        document.getElementById("loading").style.display = "flex";
 
-            const modalImage = document.getElementById('modalImage');
-            const imageHref = document.getElementById('imageHref');
-            const hrefUri = this.src.replace(/\/little/g, '');
-            modalImage.src = hrefUri;
-            imageHref.href = hrefUri;
-            const modal = new bootstrap.Modal(document.getElementById('imageModal'));
-            modal.show();
+        window.addEventListener("load", () => {
+            document.getElementById("loading").style.display = "none";
         });
-    });
-});
+    },
 
-// gen items
-function addItems() {
-    var container = document.querySelector('.js-items-add');
+    setupImageModals: function() {
+        document.querySelectorAll('img').forEach(image => {
+            image.addEventListener('click', (event) => {
+                const cardParent = event.target.closest('.card');
 
-    for (var i = 0; i < imageIndexes.length; i++) {
-        // Создание элементов
-        var colDiv = document.createElement('div');
-        colDiv.className = 'col-md-4 mb-3';
+                if (!cardParent) {
+                    return;
+                }
 
-        var cardDiv = document.createElement('div');
-        cardDiv.className = 'card';
+                const modalImage = document.getElementById('modalImage');
+                const imageHref = document.getElementById('imageHref');
+                const hrefUri = event.target.src.replace(/\/little/g, '');
+                modalImage.src = hrefUri;
+                imageHref.href = hrefUri;
 
-        var img = document.createElement('img');
-        img.src = '/image/items/little/' + imageIndexes[i];
-        img.className = 'card-img-top hover-brightness';
-        img.alt = 'Товар ' + (i + 1);
+                const modal = new bootstrap.Modal(document.getElementById('imageModal'));
+                modal.show();
+            });
+        });
+    },
 
-        var cardBody = document.createElement('div');
-        cardBody.className = 'card-body';
+    setupSearch: function() {
+        document.getElementById('searchInput').addEventListener('click', function() {
+            this.value = '';
+            var myModal = new bootstrap.Modal(document.getElementById('feedbackModal'));
+            myModal.show();
+        });
+    },
 
-        var cardTitle = document.createElement('h5');
-        cardTitle.className = 'card-title';
-        cardTitle.innerText = 'Товар ' + (i + 1);
+    addItems: function() {
+        var container = document.querySelector('.js-items-add');
 
-        var priceContainer = document.createElement('div');
-        priceContainer.className = 'price-container';
+        for (var i = 0; i < this.imageIndexes.length; i++) {
+            // Создание элементов
+            var colDiv = document.createElement('div');
+            colDiv.className = 'col-md-4 mb-3';
 
-        var priceOutline = document.createElement('img');
-        priceOutline.src = '/image/contur.png';
-        priceOutline.alt = 'Контур цены';
-        priceOutline.className = 'price-outline hover-brightness';
+            var cardDiv = document.createElement('div');
+            cardDiv.className = 'card';
 
-        var priceSpan = document.createElement('span');
-        priceSpan.className = 'price';
-        priceSpan.innerText = (1000 * (i + 1)) + ' ₽';  // Пример формирования цены
+            var img = document.createElement('img');
+            img.src = '/image/items/little/' + this.imageIndexes[i];
+            img.className = 'card-img-top hover-brightness';
+            img.alt = 'Товар ' + (i + 1);
 
-        // Сборка элементов вместе
-        priceContainer.appendChild(priceOutline);
-        // priceContainer.appendChild(priceSpan);
-        cardBody.appendChild(cardTitle);
-        cardBody.appendChild(priceContainer);
-        cardDiv.appendChild(img);
-        cardDiv.appendChild(cardBody);
-        colDiv.appendChild(cardDiv);
+            var cardBody = document.createElement('div');
+            cardBody.className = 'card-body';
 
-        container.appendChild(colDiv);
+            var cardTitle = document.createElement('h5');
+            cardTitle.className = 'card-title';
+            // cardTitle.innerText = 'Товар ' + (i + 1);
+
+            var priceContainer = document.createElement('div');
+            priceContainer.className = 'price-container';
+
+            var priceOutline = document.createElement('img');
+            priceOutline.src = '/image/contur.png';
+            priceOutline.alt = 'Контур цены';
+            priceOutline.className = 'price-outline hover-brightness';
+
+            var priceSpan = document.createElement('span');
+            priceSpan.className = 'price';
+            priceSpan.innerText = (1000 * (i + 1)) + ' ₽';  // Пример формирования цены
+
+            // Сборка элементов вместе
+            priceContainer.appendChild(priceOutline);
+            // priceContainer.appendChild(priceSpan);
+            cardBody.appendChild(cardTitle);
+            cardBody.appendChild(priceContainer);
+            cardDiv.appendChild(img);
+            cardDiv.appendChild(cardBody);
+            colDiv.appendChild(cardDiv);
+
+            container.appendChild(colDiv);
+        }
     }
-}
+};
 
-addItems();
+document.addEventListener('DOMContentLoaded', function() {
+    main.init();
+});
